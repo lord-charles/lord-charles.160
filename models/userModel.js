752b123fc649963a9786507cbfa5deca129ab31c
lord-charles <mwanikicharles226@gmail.com>
2024-06-03 22:57:26 +0300

    43

--- models/userModel.js
+++ models/userModel.js
@@ -33,6 +33,8 @@ const userSchema = new mongoose.Schema(
         "Secretariate",
         "SuperAdmin",
         "SeniorTeacher",
+        "lwd",
+        "ct",
       ],
     },
     dutyAssigned: [
