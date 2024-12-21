68f52f1635dd273bdef4321fe1997e3a9d703c17
lord-charles <mwanikicharles226@gmail.com>
2024-12-21 17:42:54 +0200

    43

--- routes/dataset.js
+++ routes/dataset.js
@@ -70,7 +70,7 @@ router.post(
   getStudentsInSchool_2023
 ); // all students in school
 router.get("/2023_data/students/:id", getSingleStudents_2023);
-router.get("/2023_data/get/learnersv2", getLearnersV2);
+router.post("/2023_data/get/learnersv2", getLearnersV2);
 router.patch("/2023_data/students/:id", updateSchoolDataFields_2023);
 router.patch("/2023_data/update/bulk", updateSchoolDataFieldsBulk);
 router.patch("/2023_data/update/bulkStates", bulkUpdateStateFields);
