45efde1808e27cb4cd132d150f16f18bce6efd63
lord-charles <mwanikicharles226@gmail.com>
2024-12-25 15:51:33 +0200

    54

--- models/update.js
+++ models/update.js
@@ -1,18 +1,42 @@
 const mongoose = require("mongoose");
 
-const learnerSchema = new mongoose.Schema({
-  isPromoted: {
-    type: Boolean,
+const learnerSchema = new mongoose.Schema(
+  {
+    year: Number,
+    state28: String,
+    stateName28: String,
+    county28: String,
+    payam28: String,
+    state10: String,
+    stateName10: String,
+    county10: String,
+    payam10: String,
+    school: String,
+    class: String,
+    code: String,
+    education: String,
+    form: Number,
+    formstream: Number,
+    gender: String,
+    dob: String,
+    age: Number,
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    eieStatus: String,
+    isPromoted: Boolean,
+    isDroppedOut: Boolean,
+    isAbsentDuringEnrolment: Boolean,
+    isValidated: Boolean,
+    isWithDisability: Boolean,
+    invalidationReason: String,
+    isDisbursed: Boolean,
+    learnerUniqueID: Number,
+    reference: String,
   },
-  isDroppedOut: {
-    type: Boolean,
-  },
-  learnerUniqueID: {
-    type: Number,
-  },
-  reference: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("update", learnerSchema);
+module.exports = mongoose.model("Update", learnerSchema);
