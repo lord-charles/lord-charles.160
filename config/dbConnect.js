257c86ea21644b37b182277073628f945870bf47
lord-charles <mwanikicharles226@gmail.com>
2025-01-13 16:51:20 +0200

    54

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = () => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect("mongodb://127.0.0.1:27018/")
+    .connect("mongodb://127.0.0.1:27017/")
     .then(() => {
       console.log("connection to db successfull...");
     })
